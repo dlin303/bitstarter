efdf4d94e7b546624c4d0efc8f912b10c7ef84d2
Ubuntu <ubuntu@ip-172-31-19-226.us-west-2.compute.internal>
2013-07-13 05:41:10 +0000

    reading index.html with web.js

--- web.js
+++ web.js
@@ -1,9 +1,25 @@
 var express = require('express');
+var fs = require('fs');
+
+//writing string out with call back.
+fs.readFile('index.html', function(err, data){
+	if(err) throw err;
+	console.log(data);
+});
+
+//to directly get string from readFileSync, include the encoding
+var index_string = fs.readFileSync('index.html', 'utf-8');
+//console.log(index_string);
+
+//to write to buffer, then get string
+var buf = fs.readFileSync('index.html');
+var buf_string = buf.toString();
+console.log(buf_string);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World Mungo Bungo 2!');
+  response.send(buf_string);
 });
 
 var port = process.env.PORT || 5000;
